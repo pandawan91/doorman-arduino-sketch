e45eb9073404d381f36abf14fe2055f99fa7a4c3
Vuong Ngo <v.ngo@cpc-baulogistik.de>
2017-10-09 10:03:22 +0200

    init

---
+++ Doorman/Debug/Doorman.cpp.d
@@ -0,0 +1,18 @@
+C:\Users\Vuong\AppData\Local\Temp\VMBuilds\Doorman\mega_atmega2560\Debug\Doorman.cpp.o: \
+ C:\Users\Vuong\AppData\Local\Temp\VMBuilds\Doorman\mega_atmega2560\Debug\Doorman.cpp \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Users\Vuong\Documents\Arduino\libraries\rfid-1.3.6\src/MFRC522.h \
+ C:\Users\Vuong\Documents\Arduino\libraries\rfid-1.3.6\src/require_cpp11.h \
+ C:\Users\Vuong\Documents\Arduino\libraries\rfid-1.3.6\src/deprecated.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\5mwmszy3.ln4\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
---
+++ Doorman/Release/Doorman.cpp.d
@@ -0,0 +1,17 @@
+C:\Users\Vuong\AppData\Local\Temp\VMBuilds\Doorman\mega_atmega2560\Release\Doorman.cpp.o: \
+ C:\Users\Vuong\AppData\Local\Temp\VMBuilds\Doorman\mega_atmega2560\Release\Doorman.cpp \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Users\Vuong\Documents\Arduino\libraries\rfid-1.3.6\src/MFRC522.h \
+ C:\Users\Vuong\Documents\Arduino\libraries\rfid-1.3.6\src/require_cpp11.h \
+ C:\Users\Vuong\Documents\Arduino\libraries\rfid-1.3.6\src/deprecated.h
